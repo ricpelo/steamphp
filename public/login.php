5bb98cc8bf5bceaddac7410c4f359160e4e0f2e2
Ricardo Pérez <ricpelo@gmail.com>
2025-11-07 20:14:34 +0100

    Crea subdirectorio css dentro de public

--- public/login.php
+++ public/login.php
@@ -5,7 +5,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="./output.css" rel="stylesheet">
+    <link href="css/output.css" rel="stylesheet">
     <title>Login</title>
 </head>
 
